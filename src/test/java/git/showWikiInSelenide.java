c0b5ca95ce953f2640eea547badc79ddbc4c73d9
Samat <kosbarmak3@gmail.com>
2024-09-19 13:56:32 +0500

    commit change hw

--- src/test/java/git/showWikiInSelenide.java
+++ src/test/java/git/showWikiInSelenide.java
@@ -1,23 +1,51 @@
 package git;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
 
-class showWikiInSelenide {
+class ShowWikiInSelenideTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+    }
 
     @Test
-    void  searchJunitExample() {
-        // open page
-        open("https://github.com/selenide/selenide");
-        $$("ul.UnderlineNav-body li a").findBy(text("Wiki")).click();
-        $("div.Layout-main").$(byText("Soft assertions")).click();
-        $(".markdown-body").shouldHave(text("JUnit5"));
+    void searchJunitExample() {
+        // open the page
+        open("/selenide/selenide");
+        // Кликнуть на вкладку Wiki
+        $("#wiki-tab").click();
+        // Найти и раскрыть боковую панель
+        $("div.Layout-sidebar").$(byText("Show 3 more pages…")).click();
+        // Найти и кликнуть на "SoftAssertions"
+        $(byTagAndText("a", "SoftAssertions")).shouldBe(visible).click();
+        // Проверить наличие заголовка "SoftAssertions" на странице
+        $(byTagAndText("h1", "SoftAssertions")).shouldBe(visible);
+        // Проверка примера кода для JUnit5
+        String junitFiveCodeExample = """
+            @ExtendWith({SoftAssertsExtension.class})
+            class Tests {
+                @Test
+                void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
 
-        sleep(5000);
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                }
+            }""";
+        // Проверить наличие примера кода на странице
+        $(".markdown-body").shouldHave(text(junitFiveCodeExample));
 
     }
 }
