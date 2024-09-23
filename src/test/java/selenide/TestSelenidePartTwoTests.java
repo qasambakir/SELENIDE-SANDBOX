f243e29d51d76098f499d2450e4e916a9bd3debd
Samat <kosbarmak3@gmail.com>
2024-09-23 16:06:26 +0500

    gitSolutionsEnterpriseTest

--- src/test/java/selenide/TestSelenidePartTwoTests.java
+++ src/test/java/selenide/TestSelenidePartTwoTests.java
@@ -19,7 +19,7 @@ public class TestSelenidePartTwoTests {
         }
 
     @Test
-    void GitSolutionsEnterpriseTest () {
+    void gitSolutionsEnterpriseTest () {
 
             // открываем страницу
         open("https://github.com");
